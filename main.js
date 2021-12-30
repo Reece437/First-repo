3004a766170bd83e75871b62a94069fc80066779
Reece437 <reecemunnings99@gmail.com>
2021-12-30 19:49:11 +0000

    make it better

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/views'));
+
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 	console.log('Request sent');
 	res.render('index');
 })
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(port, () => {
+	console.log(`listening on port ` + port)
+});
